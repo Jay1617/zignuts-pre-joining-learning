5ca5cd914d9a99123ad1cd43ff2f8457e4139381
Jay1617 <jaypatel16617@gmail.com>
2025-08-07 12:20:20 +0530

    Figma Task - Design 2

---
+++ Assignments/HTML + CSS - 2/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "google-sans": ['"Google Sans"', "sans-serif"],
+        poppins: ["Poppins", "sans-serif"],
+        volkhov: ["Volkhov", "serif"],
+        "Plus Jakarta Sans":["Plus Jakarta Sans"]
+      },
+      colors: {
+        "primary-orange": "#DF6951",
+        "primary-yellow": "#F1A501",
+        "primary-blue": "#181E4B",
+        "text-gray": "#5E6282",
+      },
+    },
+  },
+  plugins: [],
+};
